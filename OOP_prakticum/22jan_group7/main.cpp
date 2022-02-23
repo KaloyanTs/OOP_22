638cc61bf4cac44d37be33a52e572a657cd61529
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-02-23 09:30:12 +0200

    Task3 student book

--- OOP_prakticum/22jan_group7/main.cpp
+++ OOP_prakticum/22jan_group7/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <iomanip>
 #include <cstring>
+#include <cassert>
 const unsigned NAME_MAX = 32;
+const unsigned FN_MAX = 32;
 
 enum SwordType
 {
@@ -251,14 +253,160 @@ void Task2()
     deallocBook(b);
 }
 
+struct Course
+{
+    char name[NAME_MAX];
+    unsigned grade;
+};
+
 struct StudentsBook
 {
+    char name[NAME_MAX] = "";
+    char fn[FN_MAX];
+    unsigned gradesCount = 0;
+    Course *courses = nullptr;
 };
+
+void initStudentsBook(StudentsBook &sB)
+{
+    std::cout << "Enter name of the student: ";
+    std::cin.getline(sB.name, NAME_MAX);
+    std::cout << "Enter student's faculty ID: ";
+    std::cin.getline(sB.fn, FN_MAX);
+    sB.gradesCount = 0;
+    sB.courses = nullptr;
+}
+
+void addNewGrade(StudentsBook &sB, const char *c, unsigned g)
+{
+    assert(g >= 2 && g <= 6);
+    unsigned i = 0;
+    while (i < sB.gradesCount && strcmp(sB.courses[i].name, c))
+        ++i;
+    if (i < sB.gradesCount)
+    {
+        sB.courses[i].grade = g;
+        return;
+    }
+    Course *buf = new Course[sB.gradesCount + 1];
+    for (unsigned j = 0; j < sB.gradesCount; ++j)
+        buf[j] = sB.courses[j];
+    strcpy(buf[sB.gradesCount].name, c);
+    buf[sB.gradesCount++].grade = g;
+    if (sB.courses)
+        delete[] sB.courses;
+    sB.courses = buf;
+}
+
+void printCourse(const Course &c)
+{
+    std::cout << c.name << '\t' << c.grade << '\n';
+}
+
+void printStBook(const StudentsBook &sB)
+{
+    std::cout << sB.name << '\t' << sB.fn << '\n';
+    for (unsigned i = 0; i < sB.gradesCount; ++i)
+        printCourse(sB.courses[i]);
+}
+
+void burnStBook(StudentsBook &sB)
+{
+    if (sB.courses)
+        delete[] sB.courses;
+    sB.gradesCount = 0;
+    strcpy(sB.name, "");
+    strcpy(sB.fn, "");
+}
+
+void swapCourses(Course &a, Course &b)
+{
+    Course tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void sortCoursesByGrades(StudentsBook &sB)
+{
+    unsigned iMin;
+    for (unsigned i = 0; i < sB.gradesCount - 1; ++i)
+    {
+        iMin = i;
+        for (unsigned j = i + 1; j < sB.gradesCount; ++j)
+        {
+            if (sB.courses[j].grade > sB.courses[iMin].grade)
+                iMin = j;
+        }
+        swapCourses(sB.courses[i], sB.courses[iMin]);
+    }
+}
+
+double AvPerformance(const StudentsBook &sB, bool withFailed)
+{
+    if (!sB.gradesCount)
+        return 0;
+    double sum = 0;
+    unsigned count = 0;
+    for (unsigned i = 0; i < sB.gradesCount; ++i)
+    {
+        if (withFailed || sB.courses[i].grade >= 3)
+        {
+            ++count;
+            sum += sB.courses[i].grade;
+        }
+    }
+    return sum / count;
+}
+
+unsigned countOfFailed(const StudentsBook &sB)
+{
+    unsigned c = 0;
+    for (unsigned i = 0; i < sB.gradesCount; ++i)
+        c += (sB.courses[i].grade >= 3);
+    return c;
+}
+
+bool isAllowed(const StudentsBook &sB)
+{
+    unsigned failed = countOfFailed(sB);
+    return failed < 4 || (failed == 4 && AvPerformance(sB, true) >= 3);
+}
+
+bool isStipendiant(const StudentsBook &sB)
+{
+    return !countOfFailed(sB) && AvPerformance(sB, true) >= 5;
+}
 
 int main()
 {
     // Task1();
-    Task2();
+    // Task2();
+    StudentsBook sB;
+    initStudentsBook(sB);
+    addNewGrade(sB, "DIS", 3);
+    addNewGrade(sB, "IP", 5);
+    addNewGrade(sB, "IP", 6);
+    addNewGrade(sB, "DS", 2);
+    addNewGrade(sB, "ALGEBRA", 5);
+    printStBook(sB);
+    sortCoursesByGrades(sB);
+    printStBook(sB);
+    std::cout << sB.name << "\'s performance (with failed) is "
+              << std::fixed << std::setprecision(2)
+              << AvPerformance(sB, true) << '\n';
+    std::cout << sB.name << "\'s performance (without failed) is "
+              << std::fixed << std::setprecision(2)
+              << AvPerformance(sB, false) << '\n';
+    std::cout << "The student is ";
+    if (!isAllowed(sB))
+        std::cout << "NOT ";
+    std::cout << "passing the semester.\n";
+
+    std::cout << "The student is ";
+    if (!isStipendiant(sB))
+        std::cout << "NOT ";
+    std::cout << "taking scholarship.\n";
+    burnStBook(sB);
 
     return 0;
 }
