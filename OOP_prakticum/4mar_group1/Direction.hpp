7387a560ee4abeeca0dc5ae247f50642140408bf
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-03-04 15:07:26 +0200

    struct Direction declaration (and implementation)

---
+++ OOP_prakticum/4mar_group1/Direction.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+struct Direction
+{
+    int dy, dx;
+    Direction(unsigned d = 0)
+    {
+        if (!d)
+        {
+            dx = dy = 0;
+        }
+        else if (d == 1)
+        {
+            dy = -1;
+            dx = 0;
+        }
+        else if (d == 2)
+        {
+            dy = -1;
+            dx = 1;
+        }
+        else if (d == 3)
+        {
+            dy = 0;
+            dx = 1;
+        }
+        else if (d == 4)
+        {
+            dy = 1;
+            dx = 1;
+        }
+        else if (d == 5)
+        {
+            dy = 1;
+            dx = 0;
+        }
+        else if (d == 6)
+        {
+            dy = 1;
+            dx = -1;
+        }
+        else if (d == 7)
+        {
+            dy = 0;
+            dx = -1;
+        }
+        else if (d == 8)
+        {
+            dy = -1;
+            dx = -1;
+        }
+    }
+};
