0c75b0816a1a6597c0dc8525dcf96ff4c85e2464
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-03-04 15:09:28 +0200

    struct Map declaration

---
+++ OOP_prakticum/4mar_group1/Map.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "Direction.hpp"
+#include "Types.hpp"
+#include "Ship.hpp"
+#include "Cell.hpp"
+const unsigned MAP_SIZE_MAX = 12;
+
+struct Map
+{
+    unsigned n, m;
+    unsigned shipCount, shipLeft;
+    bool ready;
+    Ship *ships = nullptr;
+    Map();
+    Map(const Map &m);
+    void copy(const Map &);
+    void initMap(unsigned, unsigned, unsigned);
+    Map(unsigned _n, unsigned _m, unsigned _shipCount);
+    bool placeShip(const Ship &s);
+    Ship attack(unsigned y, unsigned x);
+    bool hitShip(unsigned y, unsigned x);
+    bool shipsLeft();
+    void print();
+    ~Map();
+};
