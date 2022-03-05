4aacf0baf68197a9c310d5110da0657acd4b3ebf
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-03-05 13:46:52 +0200

    main source file

---
+++ OOP_prakticum/4mar_group1_task2/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include "Sector.hpp"
+const unsigned EXIT = 0;
+const unsigned PRINT = 1;
+const unsigned ACQUIRE = 2;
+
+bool executeCmd(Sector &s, const char *cmd)
+{
+    if (!strcmp(cmd, "exit"))
+        return false;
+    else if (!strcmp(cmd, "print"))
+        s.print();
+    else if (!strcmp(cmd, "acquire"))
+    {
+        char buyer[NAME_MAX], bought[NAME_MAX];
+        std::cout << "Enter names of buyer company and acquired company:\n";
+        std::cin.getline(buyer, NAME_MAX);
+        std::cin.getline(bought, NAME_MAX);
+        s.acquire(buyer, bought);
+    }
+    return true;
+}
+
+int main()
+{
+    char cmd[NAME_MAX] = "";
+    Sector s;
+    s.readSector();
+    do
+    {
+        std::cout << "> ";
+        std::cin.getline(cmd, NAME_MAX);
+    } while (executeCmd(s, cmd));
+    return 0;
+}
