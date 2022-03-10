88040b536eed41c645c55eeac584d6994cf1ed86
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-03-10 23:22:34 +0200

    testing multiplication of 2 Polynomial

--- OOP_exercise/9mar/main.cpp
+++ OOP_exercise/9mar/main.cpp
@@ -7,6 +7,6 @@ int main()
     p1.fromString("5x^0 + 4x^2 - 1x^1");
     p2.fromString("5x^4 + 2x^2 + 1x^1");
     (p1 + p2).print();
-    // todo(p1 * p2).print();
+    (p1 * p2).print();
     return 0;
 }
