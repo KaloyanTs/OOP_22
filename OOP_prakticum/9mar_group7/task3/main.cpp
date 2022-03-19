3d88b08a506e7d78481ae888f91d0eff0c2b4e6e
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-03-19 13:13:54 +0200

    testing Student and Specialty

--- OOP_prakticum/9mar_group7/task3/main.cpp
+++ OOP_prakticum/9mar_group7/task3/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-
+#include "Specialty.hpp"
 
 int main()
 {
-
+    Specialty **SU;
+    SU = new Specialty *[3];
+    SU[0] = new Specialty("Computer science");
+    SU[1] = new Specialty("Informatics");
+    SU[2] = new Specialty("Software engeneering");
+    Student s[5];
+    for (unsigned i = 0; i < 5; ++i)
+    {
+        s[i].read(SU, 3);
+        std::cin.ignore();
+    }
+    for (unsigned i = 0; i < 3; ++i)
+        SU[i]->print();
+    for (unsigned i = 0; i < 3; ++i)
+        delete SU[i];
+    delete[] SU;
     return 0;
 }
