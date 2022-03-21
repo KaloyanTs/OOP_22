94a78c71a7de7abe27c5817a29a68f8ea53f64b9
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-03-21 22:28:35 +0200

    testing

--- OOP_prakticum/21mar/task1/main.cpp
+++ OOP_prakticum/21mar/task1/main.cpp
@@ -6,13 +6,21 @@ int main()
 	Store a;
 	Product p;
 	p.read();
+	std::cin.ignore();
 	a.addProduct(p);
 	p.read();
+	std::cin.ignore();
 	a.addProduct(p);
 	p.read();
+	std::cin.ignore();
 	a.addProduct(p);
 	p.read();
+	std::cin.ignore();
 	a.addProduct(p);
+	a.print();
+	a.printProducerGoods("az");
+	std::cout << "Product you want to buy:\n";
+	a.buy("apple", "az", 5, 2);
 	a.print();
 	return 0;
 }
