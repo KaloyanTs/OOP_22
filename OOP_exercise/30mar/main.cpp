2807b0e75c002845ab22b9a628db54b9ed8840b5
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-03-30 22:02:17 +0300

    testing

--- OOP_exercise/30mar/main.cpp
+++ OOP_exercise/30mar/main.cpp
@@ -4,12 +4,13 @@
 
 int main()
 {
-	Test a;
-	a >> std::cin << std::cout << a;
+	// Test a;
+	// a >> std::cin << std::cout << a;
 	String s;
 	std::cin.ignore();
 	std::cin >> s;
 	s = s + s;
 	std::cout << s << '\n';
+	std::cout << upper(s) << '\n';
 	return 0;
 }
