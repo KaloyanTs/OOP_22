bae941edcf0f7e089dbd05cfeb7febfa694fe820
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-04-18 18:18:47 +0300

    during prakticum

--- OOP_prakticum/15apr_group4/task1/University.cpp
+++ OOP_prakticum/15apr_group4/task1/University.cpp
@@ -2,14 +2,32 @@
 
 void University::copy(const University &other)
 {
+    clear();
+    capacity = other.capacity;
+    size = other.size;
+    students = new Student *[other.capacity];
+    for (unsigned i = 0; i < size; ++i)
+        students[i] = new Student(*other.students[i]);
 }
 
 void University::resize(unsigned newCap)
 {
+    if (newCap <= capacity)
+        return;
+
+    capacity = newCap;
+    Student **newArr = new Student *[capacity];
+    for (unsigned i = 0; i < size; ++i)
+        newArr[i] = students[i];
+    delete[] students;
+    students = newArr;
 }
 
 void University::clear()
 {
+    for (unsigned i = 0; i < size; ++i)
+        delete students[i];
+    delete[] students;
 }
 
 University::University(const University &other)
@@ -21,4 +39,19 @@ University::University(const University &other)
 University::~University()
 {
     clear();
+}
+
+void sort(bool (*less)(const Student &, const Student &))
+{
+    unsigned iMin;
+    for (unsigned i = 0; i < size - 1; ++i)
+    {
+        iMin = 0;
+        for (unsigned j = i + 1; j < size; ++j)
+            if (less(students[j], students[iMin]))
+                iMin = j;
+        Student *tmp = arr[iMin];
+        arr[iMin] = arr[i];
+        arr[i] = tmp;
+    }
 }
