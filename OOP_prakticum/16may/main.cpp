607cb43aab722569edd949f3c8b3fdb7429623aa
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-17 10:13:35 +0300

    new folder

--- OOP_prakticum/16may/main.cpp
+++ OOP_prakticum/16may/main.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
-#include "AmortizedCar.hpp"
 
 
 int main()
 {
-	AmortizedCar c("dfdfs", 65, "fdfsfdf",10);
-	std::cout << c << '\n';
-
-	Car* arr[] =
-	{
-		new Car,
-		new TuningCar,
-		new AmortizedCar
-	};
-
-	for (unsigned i = 0; i < 3; ++i)
-		std::cout << *arr[i] << '\n';
-
-	for (unsigned i = 0; i < 3; ++i)
-		delete arr[i];
-
-	return 0;
+    
+    return 0;
 }
